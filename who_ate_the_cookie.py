772c6bfbe70cf9abf77bb242e7301b8599c8b650
parthsonkusale <parthsonkusale27092006@gmail.com>
2026-06-02 12:47:07 +0530

    isinstance()

---
+++ who_ate_the_cookie.py
@@ -0,0 +1,12 @@
+    
+def datatype(x):
+    if isinstance(x , (int , float)):
+        print("Who ate the last cookie? It was Monica!")
+    elif isinstance(x , str):
+        print("Who ate the last cookie? It was Zach!")
+    else:
+        print("Who ate the last cookie? It was the dog!")
+        
+        
+        
+        
