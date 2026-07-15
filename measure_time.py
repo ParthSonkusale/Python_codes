1676b8f4988c4e95ce89069f703206ce7c472566
parthsonkusale <parthsonkusale27092006@gmail.com>
2026-07-15 09:38:18 +0530

    how to measure time of execution

--- measure_time.py
+++ measure_time.py
@@ -1,4 +1,21 @@
 import time 
+
+#in term of class and object 
+class timer:
+    def __init__(self):
+        self.start_time = 0
+        self.end_time   = 0
+        
+    def start(self):
+        self.start_time = time.perf_counter()
+        
+    def end(self):
+        self.end_time = time.perf_counter() - self.start_time
+        
+    def elapsed(self):
+        return(self.end_time)
+
+#simple 
 start = int(time.perf_counter())
 
 #execute some code
